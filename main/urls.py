94b5bbac830543cf8d3458616b4ca2618b3c96c9
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-09-07 01:48:13 +0500

    Mapping urls

--- main/urls.py
+++ main/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.contrib.auth import views as auth_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path("__debug__/", include("debug_toolbar.urls")),
+    path('login/',auth_view.LoginView.as_view, name='login'),
+    path('login/',include('core.urls'))
 ]
