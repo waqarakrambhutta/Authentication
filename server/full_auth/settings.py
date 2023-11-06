0dae3a503b9c8ccab140b8d62d01ce500c66c04b
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-11-07 00:24:16 +0500

    Setting the environment variables

--- server/full_auth/settings.py
+++ server/full_auth/settings.py
@@ -139,6 +139,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     
     'DEFAULT_AUTHENTICATION_CLASSES': [
+                'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     
     'DEFAULT_PERMISSION_CLASSES': [
