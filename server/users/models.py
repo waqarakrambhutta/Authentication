e8b7284e499046495249a7f72b0f87fdbfb8fd7a
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-11-08 20:52:15 +0500

    setting up AWS_SES => first attempt done

--- server/users/models.py
+++ server/users/models.py
@@ -37,16 +37,16 @@ class UserAccountManager(BaseUserManager):
 
 
 class UserAccount(AbstractBaseUser,PermissionsMixin):
+    first_name=models.CharField(max_length=255)
+    last_name=models.CharField(max_length=255)
     email = models.EmailField(
         unique=True,
         max_length=255
     )
     
-    
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
-    
 
     objects = UserAccountManager()
 
