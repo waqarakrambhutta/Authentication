3c2209443c20b937c675de4785ddd720ab624047
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-11-12 23:45:39 +0500

    Custom views for cookies.

--- server/users/views.py
+++ server/users/views.py
@@ -9,4 +9,55 @@ class CustomTokenObtainPairView(TokenObtainPairView):
     def post(self, request, *args,**kwargs):
         response = super().post(request,*args,**kwargs)
         
+        if response.status_code==200:
+            access_token = response.data.get('access')
+            refresh_token = response.data.get('refresh')
+            
+            response.set_cookie(
+                'access',
+                access_token,
+                max_age = settings.AUTH_COOKIE_ACCESS_MAX_AGE,
+                path = settings.AUTH_COOKIES_PATH,
+                secure = settings.AUTH_COOKIE_SECURE,
+                httponly = settings.AUTH_COOKIE_HTTP_ONLY,
+                samesite = settings.AUTH_COOKIE_SAMESITE,
+                )   
+        
+            response.set_cookie(
+                'refresh',
+                refresh_token,
+                max_age = settings.AUTH_COOKIE_REFRESH_MAX_AGE,
+                path = settings.AUTH_COOKIES_PATH,
+                secure = settings.AUTH_COOKIE_SECURE,
+                httponly = settings.AUTH_COOKIE_HTTP_ONLY,
+                samesite = settings.AUTH_COOKIE_SAMESITE,
+                )  
+            
+        return response 
+    
+
+class CustomTokenRefreshView(TokenRefreshView):
+    def post(self, request, *args, **kwargs):
+        refresh_token = request.COOKIES.get('refresh')
+        
+        if refresh_token:
+            request.data['refresh'] = refresh_token
+            
+        response = super().post(request,*args,**kwargs)
+        
+        if response.status_code == 200:
+            access_token = request.data.get('access')
+            
+        response.set_cookie(
+                'access',
+                access_token,
+                max_age = settings.AUTH_COOKIE_ACCESS_MAX_AGE,
+                path = settings.AUTH_COOKIES_PATH,
+                secure = settings.AUTH_COOKIE_SECURE,
+                httponly = settings.AUTH_COOKIE_HTTP_ONLY,
+                samesite = settings.AUTH_COOKIE_SAMESITE,
+            )
+        return response  
+        
+        
         
