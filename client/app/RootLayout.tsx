996a113210b2f677226226860ea3c528732276a0
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-11-25 03:29:47 +0500

    All routes set-up

---
+++ client/app/RootLayout.tsx
@@ -0,0 +1,16 @@
+import { inter } from "./layout";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Headers />
+        {children}
+      </body>
+    </html>
+  );
+}
--- client/app/auth/google/page.tsx
+++ client/app/auth/google/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Page = () => {
   return (
-    <div>Hello, World!</div>
+    <div>Google</div>
   )
 }
 
