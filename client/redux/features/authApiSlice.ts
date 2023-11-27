61705c71edc98d5f8edbf39eb197b23bd337c722
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-11-27 19:25:29 +0500

    Redux setup => errored

---
+++ client/redux/features/authApiSlice.ts
@@ -0,0 +1,21 @@
+import { apiSlice } from "../services/apiSlice";
+
+const authApiSlice = apiSlice.injectEndpoints({
+    endpoints: builder => ({
+        retrieveUser: builder.query({ // query when we've to GET the request
+            query: () => '/user/me/'
+        }),
+        socialAuthentication: builder.mutation({ //mutation when we've to POST the request
+            query: ({ provider, state, code }) => ({
+                url: `/o/${provider}/?state=${encodeURIComponent(state)}&code=${encodeURIComponent(code)}`,
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-type': 'application/x-www-form-urlencoded',
+                },
+            }),
+        }),
+    }),
+});
+
+export const {useRetrieveUser,useSocialAuthenticationMutation} = authApiSlice;
--- client/redux/store.ts
+++ client/redux/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import App from "next/app";
 import authReducer from "./features/authSlice";
+import { apiSlice } from "./services/apiSlice";
 
 export const makeStore = () =>
     configureStore({
         reducer: {
+            [apiSlice.reducerPath]: apiSlice.reducer,
             authReducer
         },
         devTools: process.env.NODE_ENV !== "production"
