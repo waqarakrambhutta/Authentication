33ece6b9e186669d8c7b2d74593b4000daf4f995
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-12-01 02:02:24 +0500

    Creating Register form

--- client/app/auth/register/page.tsx
+++ client/app/auth/register/page.tsx
@@ -1,9 +1,178 @@
-import React from 'react'
+'use client'
+
+import React from "react";
+import Link from "next/link";
+
+import { useState, ChangeEvent, FormEvent } from "react";
+import { useRegisterMutation } from "@/redux/features/authApiSlice";
 
 const Page = () => {
-  return (
-    <div>Register</div>
-  )
-}
 
-export default Page
+  const [register]=useRegisterMutation();
+
+  const [formData, setFormData] = useState({
+    first_name: "",
+    last_name: "",
+    email: "",
+    password: "",
+    re_password: "",
+  });
+
+  const { first_name, last_name, email, password, re_password } = formData;
+
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const onSubmit = (event: FormEvent<HTMLFormElement>) =>{
+    event.preventDefault();
+
+    register({ first_name, last_name, email, password, re_password })
+      .unwrap
+
+  }
+
+  return (
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <img
+          className="mx-auto h-10 w-auto"
+          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+          alt="Full Auth"
+        />
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Sign in for your account
+        </h2>
+      </div>
+
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form className="space-y-6" onSubmit={onSubmit}>
+          <div>
+            <label
+              htmlFor="first_name"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              First name
+            </label>
+            <div className="mt-2">
+              <input
+                id="first_name"
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                name="first_name"
+                type="first_name"
+                value={first_name}
+                onChange={onChange}
+                autoComplete="first_name"
+                required
+              />
+            </div>
+          </div>
+          <div>
+            <label
+              htmlFor="last_name"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Last name
+            </label>
+            <div className="mt-2">
+              <input
+                id="last_name"
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                name="last_name"
+                type="last_name"
+                value={last_name}
+                onChange={onChange}
+                autoComplete="last_name"
+                required
+              />
+            </div>
+          </div>
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Email Adderss
+            </label>
+            <div className="mt-2">
+              <input
+                id="email"
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                name="email"
+                type="email"
+                value={email}
+                onChange={onChange}
+                autoComplete="email"
+                required
+              />
+            </div>
+          </div>
+
+          <div>
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Password
+            </label>
+            <div className="mt-2">
+              <input
+                id="password"
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                name="password"
+                type="password"
+                value={password}
+                onChange={onChange}
+                autoComplete="password"
+                required
+              />
+            </div>
+          </div>
+
+          <div>
+            <label
+              htmlFor="re_password"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Confirm password
+            </label>
+            <div className="mt-2">
+              <input
+                id="re_password"
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                name="re_password"
+                type="password"
+                value={re_password}
+                onChange={onChange}
+                autoComplete="re_password"
+                required
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Sign in
+            </button>
+          </div>
+        </form>
+
+        <p className="mt-10 text-center text-sm text-gray-500">
+          Already have an account?{" "}
+          <Link
+            href="/auth/login/"
+            className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+          >
+            Login here!
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
