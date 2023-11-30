5e30ab22240bb19ed1239cbc88d2b9bd18adf49d
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-11-30 05:22:15 +0500

    Creating Api slices.

--- client/redux/features/authApiSlice.ts
+++ client/redux/features/authApiSlice.ts
@@ -1,11 +1,30 @@
+import { url } from "inspector";
 import { apiSlice } from "../services/apiSlice";
+import { METHODS } from "http";
+
+interface User {
+    first_name: string,
+    last_name: string,
+    email: string
+}
+
+interface SocialAuthArgs {
+    provider: string,
+    state: string,
+    code: string
+}
+
+interface CreateUserResponse {
+    success: boolean,
+    user: User
+}
 
 const authApiSlice = apiSlice.injectEndpoints({
     endpoints: builder => ({
-        retrieveUser: builder.query({ // query when we've to GET the request
+        retrieveUser: builder.query<User, void>({ // query when we've to GET the request
             query: () => '/user/me/'
         }),
-        socialAuthentication: builder.mutation({ //mutation when we've to POST the request
+        socialAuthentication: builder.mutation<CreateUserResponse, SocialAuthArgs>({ //mutation when we've to POST the request
             query: ({ provider, state, code }) => ({
                 url: `/o/${provider}/?state=${encodeURIComponent(state)}&code=${encodeURIComponent(code)}`,
                 method: 'POST',
@@ -15,11 +34,31 @@ const authApiSlice = apiSlice.injectEndpoints({
                 },
             }),
         }),
+        login: builder.mutation({
+            query: ({ gmail, password }) => ({
+                url: '/jwt/create/',
+                method: "POST",
+                body: { gmail, password }
+            }),
+        }),
+        register: builder.mutation({
+            query: ({ first_name, last_name, gmail, password, re_passowrd }) => ({
+                url: '/users/',
+                method: "POST",
+                body: { first_name, last_name, gmail, password, re_passowrd }
+            }),
+        }),
+        verify: builder.mutation({
+            query: () => ({
+                url: '/jwt/verify/',
+                method: "POST"
+            }),
+        }),
     }),
 });
 
 
-export const {retrieveUser,socialAuthentication} = authApiSlice.endpoints;
+export const { retrieveUser, socialAuthentication, login, register, verify } = authApiSlice.endpoints;
 
 
 
